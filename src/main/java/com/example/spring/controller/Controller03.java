76e341f6261def35087b359098203bd0f93ec66d
JaehongLIM2 <nu0044@naver.com>
2025-05-29 09:44:10 +0900

    java beans(dto)

---
+++ src/main/java/com/example/spring/controller/Controller03.java
@@ -0,0 +1,71 @@
+package com.example.spring.controller;
+
+import com.example.spring.dto.MyBean031;
+import com.example.spring.dto.MyBean032;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("main3")
+public class Controller03 {
+
+    // http://localhost:8080/main3/sub1
+    @RequestMapping("sub1")
+    public void sub1() {
+        System.out.println("sub1");
+    }
+
+    @RequestMapping("sub2")
+    public void sub2() {
+        System.out.println("sub2");
+    }
+
+    // 연습
+    // main3/sub3 요청 올 때 일하는 메소드 작성
+    @RequestMapping("sub3")
+    public void sub3() {
+        System.out.println("sub3");
+    }
+
+    // main3/sub4?param1=val1&param2=val2
+    @RequestMapping("sub4")
+    public void sub4(@RequestParam("param1") String param1,
+                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+    // main3/sub5?address=seoul&name=hong&age=28&home=sinchon
+    @RequestMapping("sub5")
+    public void sub5(@RequestParam Map<String, String> param) {
+        for (Map.Entry<String, String> entry : param.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+
+    // request parameter 를 java beans(Data Transaction Object) 으로 받기
+    // /main3/sub6?name=donald&address=la
+    // /main3/sub6?name=trump
+
+    @RequestMapping("sub6")
+    public void sub6(MyBean031 param) {
+        System.out.println("param.getName() = " + param.getName());
+        System.out.println("param.getAddress() = " + param.getAddress());
+    }
+
+    // 연습 : 
+    // /main3/sub7?name=musk&company=tesla&home=texas
+    // MyBean032 javaBean 만들어서 request parameter 받아보기
+    @RequestMapping("sub7")
+    public void sub7(MyBean032 param) {
+        System.out.println("param.getName() = " + param.getName());
+        System.out.println("param.getCompany() = " + param.getCompany());
+        System.out.println("param.getHome() = " + param.getHome());
+        System.out.println("param.getAge() = " + param.getAge());
+
+    }
+
+}
